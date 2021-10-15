a1c156f2b19789636f210a1ed3048e0ee3e4879b
Marco Filippone <m.filip@hotmail.it>
2021-10-15 05:31:00 +0200

    fix linting

--- src/test/scala/rbf/LayersSpec.scala
+++ src/test/scala/rbf/LayersSpec.scala
@@ -3,8 +3,7 @@ package rbf
 import org.scalatest._
 import flatspec._
 import matchers._
-import org.scalactic.{ TolerantNumerics, Equality }
-
+import org.scalactic.{TolerantNumerics, Equality}
 
 import breeze.linalg._
 import breeze.linalg.{DenseMatrix => BDM, DenseVector => BDV, sum}
@@ -14,7 +13,8 @@ class LayersSpec extends AnyFlatSpec with should.Matchers {
 
   val epsilon = 1e-4f
 
-  implicit val doubleEq: Equality[Double] = TolerantNumerics.tolerantDoubleEquality(epsilon)
+  implicit val doubleEq: Equality[Double] =
+    TolerantNumerics.tolerantDoubleEquality(epsilon)
 
   val inputs = DenseMatrix(
     Array(3.0, 9.0, 5.0, 6.0, 0.0),
@@ -26,17 +26,17 @@ class LayersSpec extends AnyFlatSpec with should.Matchers {
     Array(6.0, 0.0, 7.0, 7.0, 7.0),
     Array(6.0, 5.0, 9.0, 5.0, 8.0),
     Array(8.0, 5.0, 6.0, 8.0, 4.0),
-    Array(6.0, 2.0, 2.0, 4.0, 9.0),
+    Array(6.0, 2.0, 2.0, 4.0, 9.0)
   )
 
   "An RBF sampling hidden layer" should "generate weights based on inputs" in {
     val layer = Sample(4.0, 3, 42)
     val expected = DenseMatrix(
-      Array(0.0, 6.0, 0.0),  
-      Array(3.0, 0.0, 5.0),  
-      Array(2.0, 7.0, 7.0),  
-      Array(6.0, 7.0, 8.0),  
-      Array(1.0, 7.0, 6.0), 
+      Array(0.0, 6.0, 0.0),
+      Array(3.0, 0.0, 5.0),
+      Array(2.0, 7.0, 7.0),
+      Array(6.0, 7.0, 8.0),
+      Array(1.0, 7.0, 6.0)
     )
     assert(layer.weights(inputs) == expected)
   }
@@ -45,18 +45,18 @@ class LayersSpec extends AnyFlatSpec with should.Matchers {
     val layer = Sample(4.0, 3, 42)
     val trained = layer.predict(inputs)
     val expected = DenseMatrix(
-      Array(0.58561397, 0.03628523, 0.3781008 ),
+      Array(0.58561397, 0.03628523, 0.3781008),
       Array(0.49944823, 0.17808375, 0.32246802),
-      Array(0.1252962 , 0.10717152, 0.76753228),
+      Array(0.1252962, 0.10717152, 0.76753228),
       Array(0.51871833, 0.02351435, 0.45776732),
       Array(0.83434161, 0.02945586, 0.13620252),
-      Array(0.10753985, 0.4671318 , 0.42532835),
+      Array(0.10753985, 0.4671318, 0.42532835),
       Array(0.03004787, 0.85111047, 0.11884165),
-      Array(0.02363848, 0.6289966 , 0.34736492),
-      Array(0.10711055, 0.6359446 , 0.25694485),
-      Array(0.10494769, 0.75160583, 0.14344648),
+      Array(0.02363848, 0.6289966, 0.34736492),
+      Array(0.10711055, 0.6359446, 0.25694485),
+      Array(0.10494769, 0.75160583, 0.14344648)
     )
-    val diffs = (trained - expected).findAll( math.abs(_) > epsilon)
+    val diffs = (trained - expected).findAll(math.abs(_) > epsilon)
     assert(diffs.size == 0)
   }
 
